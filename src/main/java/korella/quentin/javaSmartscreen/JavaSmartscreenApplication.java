a041b59bf088451d930366a6977880ce3eb9c14d
Biologiker <quentink@gmx.de>
2023-04-09 12:19:56 +0200

    first

---
+++ src/main/java/korella/quentin/javaSmartscreen/JavaSmartscreenApplication.java
@@ -0,0 +1,13 @@
+package korella.quentin.javaSmartscreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSmartscreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSmartscreenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/korella/quentin/javaSmartscreen/JavaSmartscreenApplicationTests.java
@@ -0,0 +1,13 @@
+package korella.quentin.javaSmartscreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSmartscreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
