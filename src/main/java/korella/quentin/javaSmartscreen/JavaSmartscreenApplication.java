5368aabfc50874469894f49f4e29f4369936a188
Biologiker <quentink@gmx.de>
2023-04-09 23:38:10 +0200

    added spring boot admin panel

--- src/main/java/korella/quentin/javaSmartscreen/JavaSmartscreenApplication.java
+++ src/main/java/korella/quentin/javaSmartscreen/JavaSmartscreenApplication.java
@@ -3,11 +3,14 @@ package korella.quentin.javaSmartscreen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
 @SpringBootApplication
+@EnableAdminServer
 public class JavaSmartscreenApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaSmartscreenApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(JavaSmartscreenApplication.class, args);
+  }
 
 }
